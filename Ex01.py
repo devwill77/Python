06528f6bd2e1195392f39be517fe53f086f65aab
devwill77 <wsantos.alves77@gmail.com>
2020-01-15 00:41:43 -0300

    Create Ex01.py

---
+++ Ex01.py
@@ -0,0 +1,8 @@
+'''
+DESAFIO 01
+- Crie um script Python que leia o nome de uma pessoa e mostre
+uma mensagem de boas vindas de acordo com o valor digitado.
+'''
+nome = str(input('\033[36mQual o seu nome?\033[m '))
+
+print('\033[31mOlá\033[m {}{}{}\033[31m, muito prazer em te conhecer!\033[m'.format('\033[1;34m', nome, '\033[m'))
